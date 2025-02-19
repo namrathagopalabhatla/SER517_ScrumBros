a31de2b34733aa8eab67959370bd778d016779a0
Nisha Verma <nverma20@asu.edu>
2025-02-19 16:40:27 -0700

    Wireframe

--- Prototypes/Add Comment Prototype/content.js
+++ Prototypes/Add Comment Prototype/content.js
@@ -1,25 +1,25 @@
 function addScrumBrosMessage() {
-    const commentsSection = document.querySelector('#comments');
-    
-    if (commentsSection) {
-      if (!document.querySelector('.scrum-bros-message')) {
-        const messageDiv = document.createElement('div');
-        messageDiv.className = 'scrum-bros-message';
-        messageDiv.textContent = "Hello, we are Scrum Bro's";
+  const commentsSection = document.querySelector('#comments');
+  
+  if (commentsSection) {
+    if (!document.querySelector('.scrum-bros-message')) {
+      const messageDiv = document.createElement('div');
+      messageDiv.className = 'scrum-bros-message';
+      messageDiv.textContent = "Hello, we are Scrum Bro's";
 
-        commentsSection.insertBefore(messageDiv, commentsSection.firstChild);
-      }
+      commentsSection.insertBefore(messageDiv, commentsSection.firstChild);
     }
   }
-  
-  const observer = new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-      if (mutation.addedNodes.length) {
-        addScrumBrosMessage();
-      }
-    }
-  });
+}
 
-  observer.observe(document.body, { childList: true, subtree: true });
-  
-  addScrumBrosMessage();
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    if (mutation.addedNodes.length) {
+      addScrumBrosMessage();
+    }
+  }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
+addScrumBrosMessage();
